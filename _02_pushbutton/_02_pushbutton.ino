cdf69b0f53c239065d0e438eda9c874b65284e69
elecage <elecage@gmail.com>
2014-01-03 18:34:38 +0900

    pushbutton modified

--- _02_pushbutton/_02_pushbutton.ino
+++ _02_pushbutton/_02_pushbutton.ino
@@ -1,18 +1,28 @@
-int val = 0;
+int val1 = 0;
+int val2 = 0;
 void setup()
 {
   // put your setup code here, to run once:
   pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
   pinMode(PUSH1, INPUT_PULLUP);
+  pinMode(PUSH2, INPUT_PULLUP);
+  digitalWrite(RED_LED, LOW);
+  digitalWrite(GREEN_LED, LOW);
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  val = digitalRead(PUSH1);
-  
-  if(val == HIGH)
+  val1 = digitalRead(PUSH1);
+  val2 = digitalRead(PUSH2);
+  if(val1 == LOW)
     digitalWrite(RED_LED, HIGH);
   else
     digitalWrite(RED_LED, LOW);
+    
+  if(val2 == LOW)
+    digitalWrite(GREEN_LED, HIGH);
+  else
+    digitalWrite(GREEN_LED, LOW);
 }
